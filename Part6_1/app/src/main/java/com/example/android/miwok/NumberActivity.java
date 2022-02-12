fd835772d3c8445be66baf51fee56daa183ae3a5
parth7d2 <parth.comengineer@gmail.com>
2022-02-12 11:55:17 +0530

    Steps to become an Android developer

--- Part6_1/app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ Part6_1/app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -2,7 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -38,13 +40,8 @@ public class NumberActivity extends AppCompatActivity {
         words.add("Nine");
         words.add("Ten");
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-
-
-        for (int index = 0; index < words.size(); index++) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-        }
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemAdapter);
     }
 }
