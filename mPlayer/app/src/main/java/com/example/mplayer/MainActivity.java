5f542c79079b3bd84daabfb68fc33ae83ae889ab
parth7d2 <parth.comengineer@gmail.com>
2022-02-15 21:41:45 +0530

    Steps to become an Android developer

--- mPlayer/app/src/main/java/com/example/mplayer/MainActivity.java
+++ mPlayer/app/src/main/java/com/example/mplayer/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.mplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.samsungnote21);
+
+        Button playButton = (Button) findViewById(R.id.play_view);
+        playButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                mediaPlayer.start();
+            }
+        });
+
+        Button pauseButton = (Button) findViewById(R.id.pause_view);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
     }
 }
