88595a18cf2914fb650e7083128b426cbdb4ed21
parth7d2 <parth.comengineer@gmail.com>
2022-03-14 23:18:45 +0530

    Steps to become an Android developer

--- Part12_1/app/src/main/java/com/example/android/project_15a/EditorActivity.java
+++ Part12_1/app/src/main/java/com/example/android/project_15a/EditorActivity.java
@@ -13,6 +13,8 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.os.Bundle;
 
+import com.example.android.project_15a.data.PetContract.PetEntry;
+
 public class EditorActivity extends AppCompatActivity {
 
     /** EditText field to enter the pet's name */
@@ -69,11 +71,11 @@ public class EditorActivity extends AppCompatActivity {
                 String selection = (String) parent.getItemAtPosition(position);
                 if (!TextUtils.isEmpty(selection)) {
                     if (selection.equals(getString(R.string.gender_male))) {
-                        mGender = 1; // Male
+                        mGender = PetEntry.GENDER_MALE; // Male
                     } else if (selection.equals(getString(R.string.gender_female))) {
-                        mGender = 2; // Female
+                        mGender = PetEntry.GENDER_FEMALE; // Female
                     } else {
-                        mGender = 0; // Unknown
+                        mGender = PetEntry.GENDER_UNKNOWN; // Unknown
                     }
                 }
             }
